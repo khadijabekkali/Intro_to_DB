ea170fdb020f85f1536d4b0f71f81cd2ffdf410d
Khadija Bekkali <khadijabekkali44@gmail.com>
2025-12-14 21:15:32 +0100

    Add script to print full description of Books table (task_4.sql)

---
+++ task_4.sql
@@ -0,0 +1,18 @@
+-- Use the database (optional if passed via mysql command)
+USE alx_book_store;
+
+-- Print the full description of the 'books' table
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_DEFAULT AS 'Default',
+    COLUMN_KEY AS 'Key',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
